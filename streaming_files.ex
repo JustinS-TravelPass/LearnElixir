1a351c8c811ae88a1d1e8dd5bfaa2765e3e1707a
Justin Stoddard <justin.stoddard@travelpassgroup.com>
2025-11-06 09:47:43 -0700

    streaming files

--- streaming_files.ex
+++ streaming_files.ex
@@ -1,3 +1,27 @@
-defmodule StreamingFiles do
+defmodule Typewriter do
+  def print_file(file_path) do
+    File.stream!(file_path) |> # \n
+    Stream.map(&(String.replace(&1, "\n", ""))) |>
+    Enum.each(&Typewriter.print_line/1)
+  end
 
+  def print_line(line) do
+    line |>
+    String.split("") |>
+    Enum.each(&Typewriter.print_char/1)
+
+    IO.write("\n")
+
+    # You can use Erlang modules in Elixir.
+    # Erlang modules are prefixed with a semi-colon like Atoms.
+    :timer.sleep(500)
+  end
+
+  def print_char(char) do
+    char |> IO.write
+
+    :timer.sleep(100)
+  end
 end
+
+Typewriter.print_file("neo.txt")
