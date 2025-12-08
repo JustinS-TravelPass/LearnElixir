d90e8c1dc2d42eb5346ed1a331b3e6d61c7b8576
Justin Stoddard <justin.stoddard@travelpassgroup.com>
2025-12-08 13:39:50 -0700

    created EScript game of stones

--- game_of_stones/lib/game_of_stones/client.ex
+++ game_of_stones/lib/game_of_stones/client.ex
@@ -12,6 +12,20 @@ defmodule GameOfStones.Client do
       :world
 
   """
+  # This main function is requires to run this as EScript.
+  def main(argv) do
+    parse(argv) |> play()
+  end
+
+  # Parse is used here to parse the arguments passed to the script from the command line.
+  # Run "mix escript.build" to build the escript.
+  # Run "game_of_stones --stones=10" to start the game with 10 stones.
+  def parse(arguments) do
+    {opts, _, _} = OptionParser.parse(arguments, switches: [stones: :integer])
+    IO.inspect(opts)
+    opts |> Keyword.get(:stones, 30)
+  end
+
   def play(initial_stones_num \\ 30) do
     GameOfStones.Server.start(initial_stones_num)
     start_game!()
